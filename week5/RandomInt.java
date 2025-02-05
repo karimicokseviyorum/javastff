e7626e220553dc186f80d0eea7977be37be0a5e4
Ahmet <karimicokseviyorum@proton.me>
2025-02-05 14:08:53 +0100

    fix

--- week5/RandomInt.java
+++ week5/RandomInt.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class RandomInt {
+	public static int randomGen(int a, int b) {
+		int random = (int)(Math.random() * (b-a+1) + a);
+		return random;
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner line = new Scanner(System.in);
 		System.out.println("Enter two numbers: ");
 		int a, b;
 		a = Integer.parseInt(line.next());
-		b = Integer.parseInt(line.next());
-		System.out.println("a: " + a + "b: " + b);
+		b = Integer.parseInt(line.next()); // space kadar okuyor
+		// simdi burada random int olusturmak istiyorum
+		System.out.println(randomGen(a, b));
 	}
 }
